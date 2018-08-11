904c826ae4ef1ddf05921195c0ae23d65738d30b
alexanderchao123 <chao.alexander.123@gmail.com>
2018-08-11 16:35:40 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,12 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
-  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+  # puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 
+  colors_string = ""
+  index = 0
+
+  while index < colors.length
+      puts colors[index]
+      index += 1
+  end
 end
