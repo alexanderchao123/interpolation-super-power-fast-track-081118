8b9833487202e03434feb85dc7fed5b0bf610a32
alexanderchao123 <chao.alexander.123@gmail.com>
2018-08-11 16:42:22 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,15 +2,17 @@
 def display_rainbow(colors)
   # puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 
-  colors_string = ""
+  colored = []
   index = 0
 
   while index < colors.length
       color = colors[index]
-      puts color[0].capitalize
-      # first_letter =
+      first_letter = color[0].capitalize
+      color_format = "#{first_letter}: #{color}"
+      colored << color_format
       index += 1
   end
+  return color_format.join(",")
 end
 
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
