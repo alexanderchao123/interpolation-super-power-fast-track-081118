87fb7bbe7fb0b42bcef95a9c7473526aa1e2d593
alexanderchao123 <chao.alexander.123@gmail.com>
2018-08-11 16:51:43 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,11 +1,11 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
   # Solution 1
-  # puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 
-  colored = []
-  index = 0
-  while index < colors.length
+  # colored = []
+  # index = 0
+  # while index < colors.length
       # Solution 2
       # colored << "#{colors[index][0].capitalize}: #{colors[index]}"
 
@@ -15,9 +15,9 @@ def display_rainbow(colors)
       # color_format = "#{first_letter}: #{color}"
       # colored << color_format
 
-      index += 1
-  end
-  puts colored.join(', ')
+      # index += 1
+  # end
+  # puts colored.join(', ')
 end
 
 # colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
